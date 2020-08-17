48475cc5a2c57005702a2a2326052bf43f2ddb8b
Jenny <jennyngu@hawaii.edu>
2020-08-17 10:03:25 -1000

    Updates

---
+++ precache-manifest.fd49f1d99f168af80f7f2242d36dea39.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "94151549b5f031920ee4a136b782a7bf",
+    "url": "/portfolio/static/media/waimeavalley.94151549.PNG"
+  },
+  {
+    "revision": "9522fca84eea44d1a04d",
+    "url": "/portfolio/static/css/main.8dec981d.chunk.css"
+  },
+  {
+    "revision": "c1a95f7335cbfb730b937e19e8830a53",
+    "url": "/portfolio/static/media/programmer.c1a95f73.svg"
+  },
+  {
+    "revision": "3baa7a4ed71c4647407f",
+    "url": "/portfolio/static/js/1.3baa7a4e.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "b4797fb6336cdcd561db37b7d024aa6e",
+    "url": "/portfolio/static/media/robofriends.b4797fb6.PNG"
+  },
+  {
+    "revision": "453e9e290b5e345dce0c19f4b5927c53",
+    "url": "/portfolio/static/media/smart-brain.453e9e29.PNG"
+  },
+  {
+    "revision": "9522fca84eea44d1a04d",
+    "url": "/portfolio/static/js/main.9522fca8.chunk.js"
+  },
+  {
+    "revision": "c6c829a3caedd17295f76fc6cb12d761",
+    "url": "/portfolio/static/media/speedi.c6c829a3.PNG"
+  },
+  {
+    "revision": "a6edb3ad683fbc7cd40b381bb22e70fe",
+    "url": "/portfolio/static/media/SpeediShuttle.a6edb3ad.pdf"
+  },
+  {
+    "revision": "975d94604786a27b55c330e81c08fcd1",
+    "url": "/portfolio/static/media/tank.975d9460.PNG"
+  },
+  {
+    "revision": "e0eeac1c253cb6ba3a468c9963a1778f",
+    "url": "/portfolio/static/media/responsive.e0eeac1c.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/portfolio/static/media/learn.59389695.svg"
+  },
+  {
+    "revision": "3baa7a4ed71c4647407f",
+    "url": "/portfolio/static/css/1.73108633.chunk.css"
+  },
+  {
+    "revision": "d83e4dbf38f146aaa618158be4594a25",
+    "url": "/portfolio/index.html"
+  }
+];
